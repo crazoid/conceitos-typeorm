72aa8e4defbedfd6847412fed9179a4a0146da53
Fabio Palhano <crazoid@gmail.com>
2020-11-28 18:48:41 -0300

    fix delete

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -6,7 +6,9 @@ import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
   public async execute(id:string): Promise<void> {
+
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+
     const transaction = await transactionsRepository.findOne(id);
 
     if(!transaction){
